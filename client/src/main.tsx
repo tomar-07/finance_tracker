ca3c67d23d7e7ea394e45903e1ddf898539f53c6
Abhikalp Tomar <abhikalptomar@Abhikalps-MacBook-Air.local>
2024-07-07 02:14:03 +0530

    v1

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = "pk_test_cHJpbWFyeS1oZW4tNDkuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+import cors from "cors";
+
+const app: Express = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURI: string =
+  "mongodb+srv://abhikalptomar:tomar_07@cluster07.tluqqe8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster07";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECTED TO MONGODB!"))
+  .catch((err) => console.error("Failed to Connect to MongoDB:", err));
+
+app.use("/financial-records", financialRecordRouter);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
